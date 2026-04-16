f0843f14d8814c6a9e747086c510943031e5c4cc
Basav Bamrah <80160941+basavbamrah@users.noreply.github.com>
2026-04-16 07:51:31 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        l = len(strs)
+        if l==0:
+            return "None"
+        if l==1 and strs[0]=="":
+            return ""
+        enc_str="@ # $".join(strs)
+        return enc_str
+
+    def decode(self, s: str) -> List[str]:
+        if len(s)==0:
+            return [""]
+        if s == "None":
+            return []
+        return s.split("@ # $")
